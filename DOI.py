2a5f85efa0b3b58b1b875e181f8917f5dd7ed5e9
jsalcal <jsalcal@gmail.com>
2017-10-03 16:43:51 +0200

    Fixed zenodo upload for files bigger than 100mb

--- DOI.py
+++ DOI.py
@@ -28,12 +28,29 @@ class DOI:
         }
         return requests.post(self.zapi + "?access_token=" + self.ztoken, data=json.dumps(data), headers={"Content-Type": "application/json"})
 
-    # Upload a single file
+    # Upload a single file smaller than 100mb
     def zenodoUploadFile(self, url_files, filepath):
         self.logger.info('DOI file upload:' + str(filepath))
         data = {'filename': os.path.basename(filepath) }
         files = {'file': open(filepath, 'rb')}
-        return requests.post(url_files + "?access_token=" + self.ztoken, data=data, files=files)
+        return requests.post(url_files + "?access_token=" + self.ztoken, data=data, files=files, timeout=300)
+
+    # Upload a single file bigger than 100mb
+    def zenodoUploadFileBig(self, url_files, filepath):
+        self.logger.info('DOI BIG file upload:' + str(filepath))
+        return requests.put('%s/%s' % (url_files, os.path.basename(filepath)),
+                         data=open(filepath, 'rb'),
+                         headers={"Accept": "application/json",
+                                  "Authorization": "Bearer %s" % self.ztoken,
+                                  "Content-Type": "application/octet-stream"})
+
+    # Is file bigger than the threshold of 100mb // Zenodo limitations
+    def isFileBig(self, fname):
+        szMB = os.path.getsize(fname) >> 20
+        if szMB < 99:
+            return False
+        else:
+            return True
 
     # Run the whole upload process
     def runUpload(self):
@@ -44,12 +61,17 @@ class DOI:
         if ret.status_code < 300: # success
             # Data upload (file by file)
             for f in self.files:
-                ret = self.zenodoUploadFile(res_create['links']['files'], os.path.join(self.direc, f))
+                # Evaluate file size
+                if self.isFileBig(f):
+                    ret = self.zenodoUploadFileBig(res_create['links']['bucket'], os.path.join(self.direc, f))
+                else:
+                    ret = self.zenodoUploadFile(res_create['links']['files'], os.path.join(self.direc, f))
                 if ret.status_code < 300:  # success
                     self.logger.info('OK, file uploaded on zenodo')
                 else:
                     self.logger.error('ERR, uploading file via zenodo')
+                    self.logger.error(ret)
         else:
-            self.logger.error('ERR, uploading file via zenodo')
+            self.logger.error('ERR, preparing upload file via zenodo')
 
         return str(res_create['id'])
